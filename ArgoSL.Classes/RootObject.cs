85101cc93ee683ee41789c2799744a534aaa91db
artingoftari <artin.goftari@gmail.com>
2017-11-13 16:50:10 +0100

    revamp

--- ArgoSL.Classes/RootObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace ArgoSL.Classes
-{
-    public class RootObject
-    {
-
-    }
-}
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ArgoSL.Types;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,22 @@ namespace ArgoSL
         public Form1()
         {
             InitializeComponent();
+            GetRealTimeInfo();
+        }
+        private void GetRealTimeInfo()
+        {
+            string apiRoot = "http://api.sl.se/api2/";
+            string format = "json";
+            string key = "277124c377f64fb49754856186782460";
+            string siteId = "9509";
+            string timeWindow = "20";
+            
+
+            XHttpClient apiClient = new XHttpClient(apiRoot);
+            SLClient client = new SLClient(apiClient);
+
+            string result = client.GetRealTimeInfoForStation(key, siteId, timeWindow);
+            RealTimeInfo info = JsonConvert.DeserializeObject<RealTimeInfo>(result);
         }
     }
 }
