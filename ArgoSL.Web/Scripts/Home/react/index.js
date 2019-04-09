a649ee2e1d467bbe875abab066cc55a1331c16ba
Sascha Goftari <sascha.goftari@gmail.com>
2019-04-09 16:04:41 +0200

    Install React. It works!

---
+++ ArgoSL.Web/Scripts/Home/react/index.js
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => (
+    <>
+        <h1>React in ASP.NET MVC!</h1>
+        <div>Snart kommer ArgoSL att växa fram här :)</div>
+    </>
+);
+
+render(<App />, document.getElementById('app'));
